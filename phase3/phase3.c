d83a2b010a33f626466735283917b14bb6ebc318
Gabe <ggn@email.arizona.edu>
2024-03-19 13:08:22 -0700

    e

--- phase3/phase3.c
+++ phase3/phase3.c
@@ -9,7 +9,19 @@
 
 
 int spawn(char *name, int (*func)(char *), char *arg, int stackSize, int priority, int *pid) {
-    printf("eee\n");
+    *pid = spork(name, func, arg, stackSize, priority);
+    //printf("%d\n", pid);
+    //  If the process
+// returns, then the trampoline will automatically call Terminate() (still from
+// user mode) on behalf of this process, passing as the argument the value returned
+// from the user-main function.
+// The PID of the child is returned using an out parameter pid; the return
+// value is 0 if the child was successfully created, and -1 if not.
+
+    if (*pid < 0) {
+        return -1; 
+    }
+    return 0;
     
 }
 
