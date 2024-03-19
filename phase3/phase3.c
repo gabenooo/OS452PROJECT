f417742bfc75bbd5bdd1c854191d3a2704a966cd
KyleElison <kyleelison@arizona.edu>
2024-03-19 12:49:06 -0700

    t

--- phase3/phase3.c
+++ phase3/phase3.c
@@ -4,11 +4,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <phase1.h>
-void phase3_init(void){
 
+
+
+void phase3_init(void){
+    systemCallVec[SYS_TERMINATE] = Terminate;
 }
 
 void phase3_start_service_processes(void){
     
 }
 
+int spawn(char *name, int (*func)(char *), char *arg, int stackSize, int priority, int *pid) {
+    
+}
+
+void Terminate(int status) {
+
+}
