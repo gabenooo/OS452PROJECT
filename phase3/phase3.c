7b3cdececbd3e9c74c4f3870b85bfc593ef591a8
Gabe <ggn@email.arizona.edu>
2024-03-19 12:55:24 -0700

    wo

--- phase3/phase3.c
+++ phase3/phase3.c
@@ -5,19 +5,21 @@
 #include <string.h>
 #include <phase1.h>
 
-
+int spawn(char *name, int (*func)(char *), char *arg, int stackSize, int priority, int *pid) {
+    printf("eee\n");
+    
+}
 
 void phase3_init(void){
     systemCallVec[SYS_TERMINATE] = Terminate;
+    systemCallVec[SYS_SPAWN] = spawn;
 }
 
 void phase3_start_service_processes(void){
     
 }
 
-int spawn(char *name, int (*func)(char *), char *arg, int stackSize, int priority, int *pid) {
-    
-}
+
 
 void Terminate(int status) {
 
