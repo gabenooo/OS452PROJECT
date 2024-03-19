1ccb158af04570f4dd86d75db2dfc375b6f3b30a
KyleElison <kyleelison@arizona.edu>
2024-03-19 13:08:09 -0700

    t

--- phase3/phase3.c
+++ phase3/phase3.c
@@ -14,12 +14,30 @@ int spawn(char *name, int (*func)(char *), char *arg, int stackSize, int priorit
 
 void terminate(int status) {
     int pid = getpid();
+    int returnStatus = 0;
 
-    //while (join())
+    while (join(&returnStatus) != -2) {
+        /* continue to try and join until it is negative 2 */
+    }
+
+    quit(status);
+}
+
+int wait(int *pid, int *status) {
+    int returnStatus;
+    int joinStatus = join(status);
+
+    /* If it joined with a child then set the pid accordingly */
+    if (joinStatus >= 0 ) {
+        *pid = joinStatus;
+    }
+    
+    return joinStatus;
 }
 
 void phase3_init(void){
     systemCallVec[SYS_TERMINATE] = terminate;
+    systemCallVec[SYS_WAIT] = wait;
     systemCallVec[SYS_SPAWN] = spawn;
 }
 
