ec903cf4693deabbcb43f04f3faa64f99a191527
KyleElison <kyleelison@arizona.edu>
2024-04-02 12:57:35 -0700

    t

--- phase4a/phase4.c
+++ phase4a/phase4.c
@@ -14,24 +14,73 @@ struct sleepItem {
     struct sleepItem* next;
 };
 
-struct sleepItem sleepQueue[MAXPROC];
-
+struct sleepItem sleepItems[MAXPROC];
+struct sleepItem* sleepQueue;
 
 
 void clock() {
     USLOSS_Console("clock called\n");
     while (1 == 1) {
+        
         int status;
         int size;
         MboxRecv(CLOCK, &status, size);
         USLOSS_Console("recieved\n");
+
+        /* Unblock each process that is ready to be unblocked*/
+        if (sleepQueue != NULL) {
+            struct sleepItem* cur = sleepQueue;
+            while (cur != NULL ) {
+                if (currentTime() >= sleepQueue->wakeupTime ) {
+                    MboxSend( cur->mboxId, NULL, 0);
+                    cur = cur->next;
+                } else {
+                    break;
+                }
+            }
+        }
     }
     
 }
 
 
-void sleep(){
-    
+void sleep(void* arg){
+    USLOSS_Sysargs *args = (USLOSS_Sysargs*) arg;
+
+    int pid = curpid();
+    int mboxID = MboxCreate(0, 0);
+
+    sleepItems[pid].mboxId = mboxID;
+    sleepItems[pid].wakeupTime = (currentTime() + ((int)args->arg1 * 1000000));
+
+    /* First checks if queue is currently null */
+    if (sleepQueue == NULL) {
+        sleepQueue = &sleepItems[pid];
+    } else {
+        /* Otherwise, if the new item is quicker than current, add to front of queue */
+        struct sleepItem* cur = sleepQueue;
+        if (sleepItems[pid].wakeupTime < cur->wakeupTime) {
+            sleepItems[pid].next = cur;
+            sleepQueue = &sleepItems[pid];
+        }
+        /* Otherwise insert where it belongs in the queue */
+        while (cur->next != NULL) {
+            if (sleepItems[pid].wakeupTime < cur->next->wakeupTime ) {
+                sleepItems[pid].next = cur->next;
+                cur->next = &sleepItems[pid];
+                break;
+            } 
+            cur = cur->next;
+        }
+        /* If no position found then add to the end of the queue */
+        if (cur->next == NULL) {
+            cur->next = &sleepItems[pid];
+        }
+    }
+
+    MboxRecv(mboxID, NULL, 0);
+    args->arg4 = 0;
+
     return;
 }
 
