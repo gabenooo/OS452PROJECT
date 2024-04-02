59974997af04f1192de58a3018ef31e1c42c0646
KyleElison <kyleelison@arizona.edu>
2024-04-02 12:32:53 -0700

    T

--- phase4a/phase4.c
+++ phase4a/phase4.c
@@ -6,18 +6,27 @@
 #include <phase2.h>
 #include <phase1.h>
 
+int CLOCK = 0;
+
 struct sleepItem {
-    int procId;
     int mboxId;
     int wakeupTime;
-    struct sleepItem next;
+    struct sleepItem* next;
 };
 
 struct sleepItem sleepQueue[MAXPROC];
 
-void phase4_init(void){
 
-    return;
+
+void clock() {
+    USLOSS_Console("clock called\n");
+    while (1 == 1) {
+        int status;
+        int size;
+        MboxRecv(CLOCK, &status, size);
+        USLOSS_Console("recieved\n");
+    }
+    
 }
 
 
@@ -26,7 +35,18 @@ void sleep(){
     return;
 }
 
+void phase4_init(void){
+    for (int i = 0; i < MAXPROC; i++) {
+        sleepQueue[i].mboxId = 0;
+        sleepQueue[i].wakeupTime = 0;
+        sleepQueue[i].next = NULL;
+    }
+    return;
+}
+
 void phase4_start_service_processes(void){
     systemCallVec[SYS_SLEEP] = sleep;
+    //systemCallVec[CLOCK] = sleep;
+    spork("ClockD", clock, NULL, USLOSS_MIN_STACK, 1);
     
 }
