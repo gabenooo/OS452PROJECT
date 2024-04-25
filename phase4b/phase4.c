8cc7c28a837d053deed487fa7b4ae47394d067c1
Gabe <ggn@email.arizona.edu>
2024-04-25 10:47:05 -0700

    e

--- phase4b/phase4.c
+++ phase4b/phase4.c
@@ -85,7 +85,7 @@ void diskSize(void* arg) {
 }
 
 void diskRead(void* arg) {
-    USLOSS_Console("read\n");
+    //USLOSS_Console("read\n");
     USLOSS_Sysargs *args = (USLOSS_Sysargs*) arg;
     void * buffer = args->arg1;
     long sectors = args->arg2;
